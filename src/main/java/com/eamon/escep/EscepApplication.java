51c78cb88d597ea16ddfcccc212538a615dd43c7
eamon.zhang <>
2019-07-11 11:12:54 +0800

    init project structure

---
+++ src/main/java/com/eamon/escep/EscepApplication.java
@@ -0,0 +1,13 @@
+package com.eamon.escep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscepApplication.class, args);
+    }
+
+}
